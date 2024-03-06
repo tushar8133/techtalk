dc26237ae021ec5cdcd62562be23db4d10b14faf
tushar8133 <tushar8133@gmail.com>
2024-03-06 15:26:19 +0530

    loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AppService } from './app.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Test1Component } from './test1/test1.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { Test1Component } from './test1/test1.component';
     HeaderComponent,
     FooterComponent,
     Test1Component,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
